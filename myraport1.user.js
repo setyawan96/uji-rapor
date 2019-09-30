82978a8c11298c98131c994f503e209615eb70fe
setyawan96 <55916564+setyawan96@users.noreply.github.com>
2019-09-30 18:58:45 +0700

    Update myraport1.user.js

--- myraport1.user.js
+++ myraport1.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         myraport1
 // @namespace    https://github.com/setyawan96/raport
-// @version      1.0
+// @version      1.1
 // @description  Hanya mempermudah guru
 // @author       Wawan
 // @homepageURL  https://github.com/setyawan96/raport
